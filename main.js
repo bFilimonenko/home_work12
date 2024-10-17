bdb384510a3916a00be937f6f08493b777155ec1
bogdanafilimonenko <bfilimonenko@gmail.com>
2024-10-17 19:46:37 +0300

    feature: delete unnecessary images, improvement of all functions, cleaning the code

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 let bush = document.querySelector('.bush');
-console.log(bush);
 
-let animals = document.querySelectorAll('.animals img');
-console.log(animals);
+let animals = document.querySelectorAll('.animals div .animal');
 
 let kolobok = document.querySelector('.kolobok img');
+
+let bushShake = document.querySelectorAll('div.bush_container img');
+
+bush.addEventListener('click', function () {
+    bushShake.forEach(bush => bush.classList.toggle('active'));
+    setTimeout(function () {
+        bushShake.forEach(bush => bush.classList.toggle('active'));
+    }, 1500);
+})
 
 let currentAnimal = 0;
 
@@ -18,10 +25,29 @@ bush.addEventListener('click', function () {
     animals[currentAnimal].style.opacity = 1;
 })
 
+
+bush.addEventListener('click', function () {
+    animals[currentAnimal].classList.toggle('animated')
+    setTimeout(function () {
+        animals[currentAnimal].classList.toggle('animated')
+    }, 2000);
+
+    console.log(animals[currentAnimal]);
+})
+
+
 bush.addEventListener('click', function () {
     kolobok.classList.toggle('active');
     setTimeout(function () {
         kolobok.classList.toggle('active');
     }, 2000);
-    console.log(kolobok)
+})
+
+
+let animalText = document.querySelectorAll('.animals div .text');
+
+let animalAction = document.querySelector('.animals');
+
+animalAction.addEventListener('click', function () {
+    animalText.forEach(animal => animal.classList.toggle('active'));
 })
